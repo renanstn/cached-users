477b690b5b1e4104852fd2ab648fecf8f5e0e58a
Renan <renan_ota@hotmail.com>
2020-01-12 15:23:45 -0300

    Melhorados os helpers

--- src/helpers.py
+++ src/helpers.py
@@ -1,19 +1,19 @@
-def get_hemisphere(lat):
-    return 'norte' if float(lat) > 0 else 'sul'
+class Helpers:
 
+    def get_hemisphere(self, lat):
+        return 'norte' if float(lat) > 0 else 'sul'
 
-def get_user(user):
-    return {
-        'username': user['username'],
-        'email': user['email'],
-        'website': user['website'],
-        'hemisphere': user['hemisphere']
-    }
+    def get_user(self, user):
+        return {
+            'username': user['username'],
+            'email': user['email'],
+            'website': user['website'],
+            'hemisphere': user['hemisphere']
+        }
 
-
-def print_user(user):
-    print('email: {}\nwebsite: {}\nhemisfério: {}'.format(
-        user['email'],
-        user['website'],
-        user['hemisphere']
-    ))
+    def print_user(self, user):
+        print('email: {}\nwebsite: {}\nhemisfério: {}'.format(
+            user['email'],
+            user['website'],
+            user['hemisphere']
+        ))
--- src/main.py
+++ src/main.py
@@ -1,20 +1,23 @@
 import sys
 import csv_cache
 import api
-from helpers import get_hemisphere, get_user, print_user
+import helpers
 
 
 if __name__ == '__main__':
+
     if len(sys.argv) > 1:
+
         username = sys.argv[1]
         cache = csv_cache.CsvCache()
         api = api.Api()
+        helpers = helpers.Helpers()
 
         user_in_cache = cache.check_cache(username)
 
         if user_in_cache:
-            user = get_user(user_in_cache)
-            print_user(user)
+            user = helpers.get_user(user_in_cache)
+            helpers.print_user(user)
 
         else:
             dados = api.get_data()
@@ -24,12 +27,12 @@ if __name__ == '__main__':
                 print("o usuário que você buscou não existe")
                 sys.exit(1)
 
-            hemisphere = get_hemisphere(user['address']['geo']['lat'])
+            hemisphere = helpers.get_hemisphere(user['address']['geo']['lat'])
             user['hemisphere'] = hemisphere
-            user_data = get_user(user)
+            user_data = helpers.get_user(user)
             cache.save_cache(user_data)
 
-            print_user(user_data)
+            helpers.print_user(user_data)
 
     else:
         print("passe um username")
