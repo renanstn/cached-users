60297f2e4ee0b767ba7de6159a0817a3a2835d68
Renan <renan_ota@hotmail.com>
2020-01-12 23:57:02 -0300

    Finalizados testes da classe Api

--- src/test_api.py
+++ src/test_api.py
@@ -3,8 +3,66 @@ import src.api as api
 
 class TestApi(unittest.TestCase):
 
+    api = api.Api()
+
     def test_get_data(self):
-        pass
+        self.api.get_data()
+        self.assertTrue(isinstance(self.api.data, list))
 
     def test_find_user(self):
-        pass
+        api_data = [
+            {
+                "id": 1,
+                "name": "Leanne Graham",
+                "username": "Bret",
+                "email": "Sincere@april.biz",
+                "address": {
+                    "street": "Kulas Light",
+                    "suite": "Apt. 556",
+                    "city": "Gwenborough",
+                    "zipcode": "92998-3874",
+                    "geo": {
+                        "lat": "-37.3159",
+                        "lng": "81.1496"
+                    }
+                },
+                "phone": "1-770-736-8031 x56442",
+                "website": "hildegard.org",
+                "company": {
+                    "name": "Romaguera-Crona",
+                    "catchPhrase": "Multi-layered client-server neural-net",
+                    "bs": "harness real-time e-markets"
+                }
+            },
+            {
+                "id": 2,
+                "name": "Ervin Howell",
+                "username": "Antonette",
+                "email": "Shanna@melissa.tv",
+                "address": {
+                    "street": "Victor Plains",
+                    "suite": "Suite 879",
+                    "city": "Wisokyburgh",
+                    "zipcode": "90566-7771",
+                    "geo": {
+                        "lat": "-43.9509",
+                        "lng": "-34.4618"
+                    }
+                },
+                "phone": "010-692-6593 x09125",
+                "website": "anastasia.net",
+                "company": {
+                    "name": "Deckow-Crist",
+                    "catchPhrase": "Proactive didactic contingency",
+                    "bs": "synergize scalable supply-chains"
+                }
+            }
+        ]
+
+        self.api.data = api_data
+        user = self.api.find_user('Bret')
+
+        self.assertEqual(user['username'], 'Bret')
+        self.assertEqual(user['email'], 'Sincere@april.biz')
+        self.assertEqual(user['website'], 'hildegard.org')
+        self.assertEqual(user['address']['geo']['lat'], '-37.3159')
